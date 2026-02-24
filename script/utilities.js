fbd6d0c1c8971941fbaba1332782392613e789c9
rahmanminhaz870-crypto <rahmanminhaz870@gmail.com>
2026-02-24 16:55:27 +0600

    added dom manipulation

--- script/utilities.js
+++ script/utilities.js
@@ -1,1 +1,13 @@
-console.log('hi');
+function getCount(id) {
+    return Number(document.getElementById(id).innerText);
+}
+
+function setCount(id, value) {
+    document.getElementById(id).innerText = value;
+}
+
+function toggleTabUI(activeTab) {
+    const allTabs = document.querySelectorAll('.tab');
+    allTabs.forEach(tab => tab.classList.remove('tab-active'));
+    document.getElementById(tab - $ { activeTab }).classList.add('tab-active');
+}
